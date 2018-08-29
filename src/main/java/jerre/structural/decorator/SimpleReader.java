f684b1b8b9efc4757cef56901a5514b8e4d00225
Magnus Jerre <magnus.jerre@gmail.com>
2018-08-29 20:22:01 +0200

    Fjerner unødvendig array lagring i SimpleReader

--- src/main/java/jerre/structural/decorator/SimpleReader.java
+++ src/main/java/jerre/structural/decorator/SimpleReader.java
@@ -1,9 +1,6 @@
 package jerre.structural.decorator;
 
 public class SimpleReader implements IReader {
-
-    private char[] buffer;
-
     @Override
     public String read(char[] buffer) {
         return new String(buffer);
